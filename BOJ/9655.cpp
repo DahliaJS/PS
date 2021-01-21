719b48fdc4a688923f2d679e28c1364471b7af50
yuelrid <jinsik13579@naver.com>
2021-01-21 13:01:05 +0900

    1.21

---
+++ BOJ/9655.cpp
@@ -0,0 +1,12 @@
+//BOJ 9655 - 돌 게임
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    N % 2 ? cout << "SK" : cout << "CY";
+
+    return 0;
+}
---
+++ BOJ/9656.cpp
@@ -0,0 +1,12 @@
+//BOJ 9656 - 돌 게임 2
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    N % 2 ? cout << "CY" : cout << "SK";
+
+    return 0;
+}
